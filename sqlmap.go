83fa5707af637150b90a62cf4b4675149a44c5c6
JasonLee <limingze610@gmail.com>
2017-12-05 10:01:48 +0800

    init test

---
+++ sqlmap.go
@@ -0,0 +1,8 @@
+package gosqlmap
+
+import "fmt"
+
+
+func version() {
+    fmt.Println("gosqlmap 1.0")
+}
---
+++ test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/luyucia/gomapsql"
+
+
+func main() {
+    fmt.Println(gomapsql.version())
+}
